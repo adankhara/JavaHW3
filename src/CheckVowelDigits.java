dfe5f4f3c9c520c0b4552ba7d3a11d4b5b680f0e
dankharaasha <dankharaasha@gmail.com>
2018-12-05 10:57:15 +0000

    done

--- src/CheckVowelDigits.java
+++ src/CheckVowelDigits.java
@@ -1,46 +1,54 @@
 import java.util.Scanner;
 
 public class CheckVowelDigits {
+    int digits;
+    int vowels;
+    float digitPercentage;
+    float vowelPercentage;
+
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string:");
         String s = scanner.next();
 
-        float percentage = 100 * checkDigits(s)/s.length();
-        float percentage1 = 100*checkVowels(s)/s.length();
-        System.out.printf("Number of digits: %2s, (%5f)\n", checkDigits(s), percentage);
-        System.out.printf("Number of vowels: %2s, (%5f)", checkVowels(s), percentage1);
+
+        CheckVowelDigits cwd = new CheckVowelDigits();
+        cwd.calculate(s);
+
+        System.out.printf("Number of digits: %2d, (%5f)\n", cwd.digits, cwd.digitPercentage);
+        System.out.printf("Number of vowels: %2d, (%5f)", cwd.vowels, cwd.vowelPercentage);
     }
-    private static float checkVowels(String input)
-    {
-      int countVowel = 0;
 
-      for (int i=0;i<input.length();i++)
-      {
-          char vowel = input.charAt(i);
-          if (vowel=='a'|| vowel=='A' || vowel=='e' || vowel=='E' || vowel=='i' || vowel=='I' || vowel=='o' || vowel=='O' || vowel=='u' || vowel=='U')
-          {
-              countVowel++;
-          }
-      }
 
-      return countVowel;
-    }
-    private static float checkDigits(String input)
-    {
-        int countDigit = 0;
-
-        for (int i=0;i<input.length();i++)
-        {
-            char digit = input.charAt(i);
-            if (digit>='0' && digit<='9')
-            {
-                countDigit++;
+    public void calculate(String input) {
+        if (input.length() == 0) {
+            return;
+        }
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (isVowel(c)) {
+                vowels++;
+            } else if (isDigit(c)) {
+                digits++;
             }
         }
-
-        return countDigit;
+        vowelPercentage = vowels * 100.0f / input.length();
+        digitPercentage = digits * 100.0f / input.length();
     }
 
+    private boolean isVowel(char vowel) {
+        if (vowel == 'a' || vowel == 'A' || vowel == 'e' || vowel == 'E' || vowel == 'i' || vowel == 'I' || vowel == 'o' || vowel == 'O' || vowel == 'u' || vowel == 'U') {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isDigit(char digit) {
+        if (digit >= '0' && digit <= '9') {
+            return true;
+        }
+        return false;
+    }
 }
+
